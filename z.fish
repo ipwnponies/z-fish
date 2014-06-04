c855d25760518e942891540a5485451916822cf6
leiser <leiserfg@gmail.com>
2014-06-04 12:56:46 -0400

    fixed a bad status error

--- z.fish
+++ z.fish
@@ -21,11 +21,11 @@ end
 
 function z -d "Jump to a recent directory."
     
-    set __Z_DATA "$HOME/.z"
+    set -l __Z_DATA "$HOME/.z"
+    touch $__Z_DATA
 
     # add entries
     if [ "$argv[1]" = "--add" ]
-        touch $__Z_DATA
         set -e argv[1]
 
         # $HOME isn't worth matching
@@ -183,27 +183,3 @@ function z -d "Jump to a recent directory."
 end
 
 
-function __complete_z
-    set __Z_DATA "$HOME/.z"
-
-    awk -v q=(commandline| sed 's|^comandline ||') -F"|" '
-          BEGIN {
-        if( q == tolower(q) ) imatch = 1
-        split(substr(q, 3), fnd, " ")
-    }
-    {
-        if( imatch ) {
-            for( x in fnd ) tolower($1) !~ tolower(fnd[x]) && $1 = ""
-        } else {
-            for( x in fnd ) $1 !~ fnd[x] && $1 = ""
-        }
-        if( $1 ) print $1
-    }
-    ' $__Z_DATA 2>/dev/null
-end
-
-complete -f -c z -s t  --description 'goes to most recently accessed dir matching query'
-complete -f -c z -s l  --description 'list all dirs matching query (by frecency)'
-complete -f -c z -s r  --description 'goes to highest ranked dir matching query'
-
-complete -f -c z -a '(__complete_z)' --description 'z completer'
