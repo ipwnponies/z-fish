553a666a8f2335c7c2e56c582c4db69eeb9955bd
leiser <leiserfg@gmail.com>
2014-06-23 16:06:19 -0400

    fixed z completion

--- z.fish
+++ z.fish
@@ -61,123 +61,122 @@ function z -d "Jump to a recent directory."
 
         command mv -f $tempfile $__Z_DATA
 
-        else
-            # list/go
-            set -l last ''
-            set -l list 0
-            set -l typ ''
-            set -l fnd ''
+    else
+        # list/go
+        set -l last ''
+        set -l list 0
+        set -l typ ''
+        set -l fnd ''
 
-            while [ (count $argv) -gt 0 ]
-                switch "$argv[1]"
-                    case -- '-h'
-                        echo "z [-h][-l][-r][-t] args" >&2
-                        return
-                    case -- '-l'
-                        set list 1
-                    case -- '-r'
-                        set typ "rank"
-                    case -- '-t'
-                        set typ "recent"
-                    case -- '--'
-                        while [ "$argv[1]" ]
-                            set -e argv[1]
-                            set fnd "$fnd $argv[1]"
-                        end
-                    case '*'
+        while [ (count $argv) -gt 0 ]
+            switch "$argv[1]"
+                case -- '-h'
+                    echo "z [-h][-l][-r][-t] args" >&2
+                    return
+                case -- '-l'
+                    set list 1
+                case -- '-r'
+                    set typ "rank"
+                case -- '-t'
+                    set typ "recent"
+                case -- '--'
+                    while [ "$argv[1]" ]
+                        set -e argv[1]
                         set fnd "$fnd $argv[1]"
-                end
-                set last $1
-                set -e argv[1]
+                    end
+                case '*'
+                    set fnd "$fnd $argv[1]"
             end
+            set last $1
+            set -e argv[1]
+        end
 
-            [ "$fnd" ]; or set list 1
+        [ "$fnd" ]; or set list 1
 
-            # if we hit enter on a completion just go there
-            [ -d "$last" ]; and cd "$last"; and return
+        # if we hit enter on a completion just go there
+        [ -d "$last" ]; and cd "$last"; and return
 
-            # no file yet
-            [ -f "$__Z_DATA" ]; or return
+        # no file yet
+        [ -f "$__Z_DATA" ]; or return
 
-			set -l tempfile (command mktemp $__Z_DATA.XXXXXX)
-			test -f $tempfile; or return
+        set -l tempfile (command mktemp $__Z_DATA.XXXXXX)
+        test -f $tempfile; or return
 
-            set -l target (command awk -v t=(date +%s) -v list="$list" -v typ="$typ" -v q="$fnd" -v tmpfl="$tempfile" -F"|" '
-                function frecent(rank, time) {
-                # relate frequency and time
-                dx = t - time
-                if( dx < 3600 ) return rank * 4
-                if( dx < 86400 ) return rank * 2
-                if( dx < 604800 ) return rank / 2
-                return rank / 4
+        set -l target (command awk -v t=(date +%s) -v list="$list" -v typ="$typ" -v q="$fnd" -v tmpfl="$tempfile" -F"|" '
+            function frecent(rank, time) {
+            # relate frequency and time
+            dx = t - time
+            if( dx < 3600 ) return rank * 4
+            if( dx < 86400 ) return rank * 2
+            if( dx < 604800 ) return rank / 2
+            return rank / 4
+        }
+        function output(files, out, common) {
+            # list or return the desired directory
+            if( list ) {
+                cmd = "sort -n >&2"
+                for( x in files ) {
+                    if( files[x] ) printf "%-10s %s\n", files[x], x | cmd
+                }
+                if( common ) {
+                    printf "%-10s %s\n", "common:", common > "/dev/stderr"
+                }
+            } else {
+                if( common ) out = common
+                print out
             }
-            function output(files, out, common) {
-                # list or return the desired directory
-                if( list ) {
-                    cmd = "sort -n >&2"
-                    for( x in files ) {
-                        if( files[x] ) printf "%-10s %s\n", files[x], x | cmd
-                    }
-                    if( common ) {
-                        printf "%-10s %s\n", "common:", common > "/dev/stderr"
-                    }
-                } else {
-                    if( common ) out = common
-                    print out
+        }
+        function common(matches) {
+            # find the common root of a list of matches, if it exists
+            for( x in matches ) {
+                if( matches[x] && (!short || length(x) < length(short)) ) {
+                    short = x
                 }
             }
-            function common(matches) {
-                # find the common root of a list of matches, if it exists
-                for( x in matches ) {
-                    if( matches[x] && (!short || length(x) < length(short)) ) {
-                        short = x
-                    }
-                }
-                if( short == "/" ) return
-                # use a copy to escape special characters, as we want to return
-                # the original. yeah, this escaping is awful.
-                clean_short = short
-                gsub(/[\(\)\[\]\|]/, "\\\\&", clean_short)
-                for( x in matches ) if( matches[x] && x !~ clean_short ) return
-                return short
+            if( short == "/" ) return
+            # use a copy to escape special characters, as we want to return
+            # the original. yeah, this escaping is awful.
+            clean_short = short
+            gsub(/[\(\)\[\]\|]/, "\\\\&", clean_short)
+            for( x in matches ) if( matches[x] && x !~ clean_short ) return
+            return short
+        }
+        BEGIN { split(q, words, " "); hi_rank = ihi_rank = -9999999999 }
+        {
+            if( typ == "rank" ) {
+                rank = $2
+            } else if( typ == "recent" ) {
+                rank = $3 - t
+            } else rank = frecent($2, $3)
+            matches[$1] = imatches[$1] = rank
+            for( x in words ) {
+                if( $1 !~ words[x] ) delete matches[$1]
+                if( tolower($1) !~ tolower(words[x]) ) delete imatches[$1]
             }
-            BEGIN { split(q, words, " "); hi_rank = ihi_rank = -9999999999 }
-            {
-                if( typ == "rank" ) {
-                    rank = $2
-                } else if( typ == "recent" ) {
-                    rank = $3 - t
-                } else rank = frecent($2, $3)
-                matches[$1] = imatches[$1] = rank
-                for( x in words ) {
-                    if( $1 !~ words[x] ) delete matches[$1]
-                    if( tolower($1) !~ tolower(words[x]) ) delete imatches[$1]
-                }
-                if( matches[$1] && matches[$1] > hi_rank ) {
-                    best_match = $1
-                    hi_rank = matches[$1]
-                } else if( imatches[$1] && imatches[$1] > ihi_rank ) {
-                    ibest_match = $1
-                    ihi_rank = imatches[$1]
-                }
+            if( matches[$1] && matches[$1] > hi_rank ) {
+                best_match = $1
+                hi_rank = matches[$1]
+            } else if( imatches[$1] && imatches[$1] > ihi_rank ) {
+                ibest_match = $1
+                ihi_rank = imatches[$1]
             }
-            END {
-                # prefer case sensitive
-                if( best_match ) {
-                    output(matches, best_match, common(matches))
-                } else if( ibest_match ) {
-                    output(imatches, ibest_match, common(imatches))
-                }
+        }
+        END {
+            # prefer case sensitive
+            if( best_match ) {
+                output(matches, best_match, common(matches))
+            } else if( ibest_match ) {
+                output(imatches, ibest_match, common(imatches))
             }
-            ' $__Z_DATA)
-            
+        }
+        ' $__Z_DATA)
+        
 
-            if [ $status -gt 0 ]
-                command rm -f "$tempfile"
-            else
-                command mv -f "$tempfile" "$datafile"
-                [ "$target" ]; and cd "$target"
-            end
+        if [ $status -gt 0 ]
+            command rm -f "$tempfile"
+        else
+            command mv -f $tempfile $__Z_DATA
+            [ "$target" ]; and cd "$target"
         end
     end
 end
